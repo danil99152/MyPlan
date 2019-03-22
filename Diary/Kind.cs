870b803f2050fff74193fd7692be0afe24de776c
Danil Komyshev <danil.komyshev777@gmail.com>
2019-03-23 01:40:35 +0400

    upd

---
+++ Diary/Kind.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+
+namespace Diary
+{
+    public class Kind  : IKind<long>
+    {
+        public virtual long Id { get; set; }
+
+        public virtual string Name { get; set; }
+
+        public virtual ICollection<User> Users { get; set; }
+
+        public Kind()
+        {
+            Users = new List<User>();
+        }
+
+        public Kind(string kindName) : this()
+        {
+            Name = kindName;
+        }
+    }
+}
---
+++ Diary/Note.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diary
+{
+    public class Note
+    {
+        public virtual long Id { get; set; }
+
+        [Display(Name = "Заголовок")]
+        public virtual string Title { get; set; }
+
+        [Display(Name = "Текст")]
+        public virtual string Text { get; set; }
+
+        [Display(Name = "Дата создания заметки")]
+        public virtual DateTime CreationDate { get; set; }
+
+        [Display(Name = "Дата изменения заметки")]
+        public virtual DateTime ChangeDate { get; set; }
+
+        [Display(Name = "Автор")]
+        public virtual User Author { get; set; }
+
+        [Display(Name = "Автор")]
+        public virtual string Place { get; set; }
+
+        public virtual ICollection<Kind> Kinds { get; set; }
+
+        public Note()
+        {
+            Kinds = new List<Kind>();
+        }
+    }
+}
